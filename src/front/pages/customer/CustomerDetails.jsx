4213c7ce3415a4a96891406f5ef3af485f986f6e
cinjude <cintilmonj@gmail.com>
2026-03-29 09:19:03 -0400

    fixed the bugs in customerd details

--- src/front/pages/customer/CustomerDetails.jsx
+++ src/front/pages/customer/CustomerDetails.jsx
@@ -11,36 +11,38 @@ export default function CustomerDetails() {
     const navigate = useNavigate()
     const { store } = useGlobalReducer()
 
-    const [customer, setCustomer] = useState([])
+    const [customer, setCustomer] = useState(null)
     const [loadingCustomers, setLoadingCustomers] = useState(false);
 
     const getSingleCustomer = async () => {
         setLoadingCustomers(true);
 
-        const token = store.token || localStorage.getItem(token)
+        const token = store?.token || localStorage.getItem('token')
+        if (!token) return;
 
         try {
-            const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/customer/${id}`, {
+            const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/customer/${id}`, {
                 headers: {
                     'Content-Type': 'application/json',
                     'Authorization': `Bearer ${token}`
                 }
             })
 
-            const data = await response.data
-            setCustomer(data)
+            const data = await response.json()
+            setCustomer(data.customer)
 
         } catch (error) {
             console.error("error retrieving customer id", id, error)
+        } finally {
+            setLoadingCustomers(false)
         }
     }
 
     useEffect(() => {
         getSingleCustomer()
-    }, [])
+    }, [store.token])
 
-    console.log("Single customer", customer)
-
+    console.log("Single customer array", customer)
 
     return (
         <div className="container bg-white">
