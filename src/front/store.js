97a11c7fa268d3709f8de9d376684486f5b281c6
cinjude <cintilmonj@gmail.com>
2026-06-10 22:45:19 -0400

    add key invoices, invoistat in global reducer

--- src/front/store.js
+++ src/front/store.js
@@ -17,13 +17,16 @@ export const initialStore = () => {
      token: localStorage.getItem("token") || null,
      customers:[],
      services:[],
-     servicesStats:null
+     servicesStats:null,
+     invoices:[],
+     invoiceStats:null
   };
   
 };
 
 export default function storeReducer(store, action = {}) {
   switch (action.type) {
+    
     case "login-provider":
       const { provider, token } = action.payload;
       localStorage.setItem("token", token); 
@@ -32,7 +35,7 @@ export default function storeReducer(store, action = {}) {
         provider,
         token,
       };
-      
+
     case "logout":
       localStorage.removeItem("token");
       return {
@@ -67,6 +70,25 @@ export default function storeReducer(store, action = {}) {
         ...store,
         servicesStats: action.payload,
       };
+
+    case "set-invoices":
+        return{ ...store, invoices: action.payload};
+
+    case "set-invoice-stats":
+         return{ ...store, invoiceStats: action.payload};
+
+    case "add-invoice":
+      return{ ...store, invoices: [action.payload, ...store.invoices] };
+    
+    case "update-invoice":
+      return{ ...store, 
+          invoices : store.invoices.map(i => i.id === action.payload.id ? action.payload : i)
+      };
+
+    case "delete-invoice":
+      return{ ...store, invoices: store.invoices.filter( i => i.id !== action.payload ) };
+
+
       
     default:
       throw Error("Unknown action.");
